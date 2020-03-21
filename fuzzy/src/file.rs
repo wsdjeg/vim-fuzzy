dc6d376034037e12256c1081b7a9365a8f91c51d
Shidong Wang <wsdjeg@outlook.com>
2020-03-21 20:59:27 +0800

    Update fuzzy

--- fuzzy/src/file.rs
+++ fuzzy/src/file.rs
@@ -9,6 +9,7 @@ pub fn fuzzy_match(files: Vec<String>, query: &str) -> Vec<String> {
             with_one_char.push(file);
         }
     }
+    rst.append(&mut with_one_char);
     rst
 }
 
@@ -22,7 +23,25 @@ fn match_file_head(file: String, query: &str) -> bool {
 #[allow(dead_code, unused_variables)]
 fn match_one_char(file: String, query: &str) -> bool {
     let mut fuck: Vec<&str> = file.split("\\").collect::<Vec<_>>();
-    fuck.pop().unwrap().starts_with(query)
+    let mut m = true;
+    let f_name = fuck.pop().unwrap();
+    let mut idx = 0;
+    for c in query.chars() {
+
+         let o = find_start_at(f_name, idx, c);
+         m = o.is_some();
+         if m {
+             idx = o.unwrap();
+         }else{
+             break
+         }
+    }
+    m
+}
+
+
+fn find_start_at(slice: &str, at: usize, pat: char) -> Option<usize> {
+    slice[at..].find(pat).map(|i| at + i)
 }
 
 // #[cfg(windows)]
