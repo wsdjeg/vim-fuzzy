6c951cdcce87ca8fc24c43e12413fd4588578369
Shidong Wang <wsdjeg@outlook.com>
2020-03-21 18:48:05 +0800

    Update

--- fuzzy/src/file.rs
+++ fuzzy/src/file.rs
@@ -1,9 +1,31 @@
+#[allow(dead_code)]
 pub fn fuzzy_match(files: Vec<String>, query: &str) -> Vec<String> {
     let mut rst: Vec<String> = Vec::new();
+    let mut with_one_char: Vec<String> = Vec::new();
     for file in files {
-        if file.contains(query) {
+        if match_file_head(file.clone(), query) {
             rst.push(file);
+        } else if match_one_char(file.clone(), query) {
+            with_one_char.push(file);
         }
     }
     rst
 }
+
+// #[cfg(not(windows))]
+#[allow(dead_code, unused_variables)]
+fn match_file_head(file: String, query: &str) -> bool {
+    let mut fuck: Vec<&str> = file.split("\\").collect::<Vec<_>>();
+    fuck.pop().unwrap().starts_with(query)
+}
+
+#[allow(dead_code, unused_variables)]
+fn match_one_char(file: String, query: &str) -> bool {
+    let mut fuck: Vec<&str> = file.split("\\").collect::<Vec<_>>();
+    fuck.pop().unwrap().starts_with(query)
+}
+
+// #[cfg(windows)]
+// fn match_file_head(file: String, query: &str) -> bool {
+// false
+// }
