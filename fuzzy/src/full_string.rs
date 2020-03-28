e9bf0159be3f79a0aae347d1bd0391761ab08024
Shidong Wang <wsdjeg@outlook.com>
2020-03-28 13:29:39 +0800

    Update

--- fuzzy/src/full_string.rs
+++ fuzzy/src/full_string.rs
@@ -29,12 +29,19 @@ fn sort_word_by_distance(words: Vec<String>, query: String) -> Vec<String> {
 // ref: http://www.voidcn.com/article/p-xgnydwfz-bqy.html
 
 // 编辑距离算法
+// https://www.dreamxu.com/books/dsa/dp/edit-distance.html
 
 fn get_distance(s1: String, s2: String) -> i32 {
-    let chars1 = s1.chars().collect::<Vec<char>>();
-    let chars2 = s2.chars().collect::<Vec<char>>();
     let len1 = s1.chars().count();
     let len2 = s2.chars().count();
+    // 当 len 为 0 时：
+    if len1 == 0 {
+        return len2 as i32;
+    }else if len2 == 0{
+        return len1 as i32;
+    }
+    let chars1 = s1.chars().collect::<Vec<char>>();
+    let chars2 = s2.chars().collect::<Vec<char>>();
     let mut d = vec![vec![0i32; len1 + 1]; len2 + 1];
     for i in 0..len1 {
         d[i][0] = i as i32;
@@ -70,4 +77,10 @@ fn similarity(s1: String, s2: String) -> f32 {
     let long = max(s1.chars().count(), s2.chars().count()) as f32;
     let distance = get_distance(s1, s2) as f32;
     1 as f32 - distance / long
+}
+
+#[allow(dead_code)]
+pub fn main() {
+    println!("{}", get_distance("hello".to_string(), "hell".to_string()));
+    
 }
