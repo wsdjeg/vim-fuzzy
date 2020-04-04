905bd903fae6452294f88d393056c010d72f1039
Shidong Wang <wsdjeg@outlook.com>
2020-04-05 00:16:45 +0800

    Update

--- fuzzy/src/bin/collect_tags.rs
+++ fuzzy/src/bin/collect_tags.rs
@@ -1,3 +1,29 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
 pub fn main() {
-    println!("{}", "hello");
+    let args: Vec<String> = std::env::args().collect();
+    let rtps: Vec<&str> = args[1].split(',').collect();
+    for a in rtps {
+        let mut fuck = a.to_string();
+        fuck.push_str("/doc/tags");
+        let f = read_username_from_file(fuck.as_str());
+        match f {
+            Ok(f) => {
+                for line in f.lines() {
+                    if let Some(first_word) = line.trim().split_whitespace().next() {
+                        println!("{}", first_word);
+                    }
+                }
+            },
+            _ => {}
+        }
+    }
+}
+
+fn read_username_from_file(fname: &str) -> Result<String, io::Error> {
+    let mut f = File::open(fname)?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
